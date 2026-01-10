d0f9982cdeea7b3e2896dd9cba24d9b1bfdc5eaf
yash chotaliya <yash.chotalia05@gmail.com>
2026-01-10 18:48:50 +0530

    Add Genre management, Register and Issue Book components

--- Backend/adminBackend.js
+++ Backend/adminBackend.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ['http://localhost:5174', 'http://localhost:5000'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5000'],
     credentials: true,
   })
 );
